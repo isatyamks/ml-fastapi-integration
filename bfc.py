40dce604a4344b063be87dcb000afc646b3858c8
Satyam Kumar <isatyamks@gmail.com>
2024-03-24 22:05:22 +0530

    Update bfc.py

--- bfc.py
+++ bfc.py
@@ -14,10 +14,18 @@ def create_folders(base_directory, folder_names):
             os.makedirs(folder_path)
             print(f"Folder '{folder_name}' created at: {folder_path}")
         else:
-            print(f"Folder '{folder_name}' already exists at: {folder_path}")
+            print(f"Folder '{python}' already exists at: {d:/python/python projects}")
+
+          
+
+        
 
 
-base_directory = "C:/YourBaseDirectory"  # Change this to your desired base directory
+base_directory = "C:/YourBaseDirectory"
+
 folder_names = ["Folder1", "Folder2", "Folder3","Folder4","Folder5","Folder6","Folder7"]
 
 create_folders(base_directory, folder_names)
+
+
+
